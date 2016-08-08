c9b1adf1c1c6437a3f71ad42e6f4ee43e01a8ceb
parhaml <lee.p.parham@gmail.com>
2016-08-08 11:12:39 -0500

    ex5

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,40 @@
+# prints the string
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+#prints string and math: 30
+puts "Hens #{25 + 30 / 6.0}"
+#prints string and math: 97
+puts "Roosters #{100 - 25 * 3.0 % 4}"
 
+#prints string
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints math: 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
 
+#prints string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
-puts 3 + 7 < 5 - 7
+#prints boolean for 5 < -2: false
+puts 3 + 2.0 < 5.0 - 7
 
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+#prints string and math: 5
+puts "What is 3 + 2? #{3.0 + 2}"
 
+#prints string and math: -2
+puts "What is 5 - 7? #{5.0 - 7}"
+
+#prints string
 puts "Oh, that's why it's false."
 
+#prints string
 puts "How about some more."
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+#prints string and boolean: true
+puts "Is it greater? #{5.0 > -2}"
+
+#prints string and boolean: true
+puts "Is it greater or equal? #{5.0 >= -2}"
+
+#prints string and boolean: false
+puts "Is it less or equal? #{5.0 <= -2}"
