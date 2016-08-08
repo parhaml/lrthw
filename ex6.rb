c76fbfe19a42bf7e010cb205d484794340f1e4df
parhaml <lee.p.parham@gmail.com>
2016-08-08 14:04:57 -0500

    ex6

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# set variable to integer value
+types_of_people = 10
+
+# set variable of string plus string interpolation of variable
+x = "There are #{types_of_people} types of people."
+
+#sets two variables to string values
+binary = "binary"
+do_not = "don't"
+
+# sets variable to string value with interpolated variables
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints these as strings
+puts x
+puts y
+
+# prints string with interpolation of a string with interpolation
+puts "I said: #{x}"
+puts "I also said: #{y}"
+
+# sets boolean
+hilarious = false
+
+# prints string with boolean as string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#prints varaibles info as string
+puts joke_evaluation
+
+# sets two variables
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# puts one string right after the other
+puts w + e
