52578f2b496424f35c2e09ed6a0edc6a662c2481
parhaml <lee.p.parham@gmail.com>
2016-08-08 14:21:19 -0500

    ex7

---
+++ ex7.rb
@@ -0,0 +1,29 @@
+# prints each to a new line
+puts "Mary had a little lamb."
+
+# this line interpolates the string 'snow' into the string line
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 #what'd that do?
+
+# it does the string 10 times
+
+# establish 12 different variables to string values
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# prints first 6 variables with no spaces and no new line character
+print end1 + end2 + end3 + end4 + end5 + end6
+
+# puts the rest of the variables on line
+puts end7 + end8 + end9 + end10 + end11 + end12
